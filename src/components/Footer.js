88c04ae76c13f8eb898e18a75a5dddf0d80037eb
Justus <galaxycheesemc@gmail.com>
2023-04-06 18:09:24 -0700

    Setting up components

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../styles/footer.css"
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; 2023 Justus Schoppmann</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Project.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/project.css';
+
+function Project(){
+    return(
+        <div className = "container">
+            <div className = "card">Project 1</div>
+            <div className = "card">Project 2</div>
+            <div className = "card">Project 3</div>
+            <div className = "card">Project 4</div>
+        </div>
+    );
+}
+export default Project;
