3bb602a8d4db8094bebb28fb2171e13b0cf089ef
Justus <galaxycheesemc@gmail.com>
2023-05-07 16:59:11 -0700

    Finished timeline

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -38,9 +38,7 @@ export const projects = [
 ];
 
 export const TimeLineData = [
-  { year: 2017, text: 'Started my journey', },
-  { year: 2018, text: 'Worked as a freelance developer', },
-  { year: 2019, text: 'Founded JavaScript Mastery', },
-  { year: 2020, text: 'Shared my projects with the world', },
-  { year: 2021, text: 'Started my own platform', },
+  { year: 2021, text: 'Started my journey', },
+  { year: 2022, text: 'Went to ASU for computer science', },
+  { year: 2023, text: 'Switched to UofA and got my Full Stack Certificate', },
 ];
